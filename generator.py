4fa5c43433d3ef5474a619b947436ec0097ddf73
daniel <vdanny911@gmail.com>
2026-04-25 14:24:10 -0500

    extractedCode function to strip response from anthropic.

--- generator.py
+++ generator.py
@@ -11,6 +11,34 @@ def generate_code():
         max_tokens = 1024,
         messages = [{"role": "user", "content": "You are a python code generator. Generate clean valid code to print hello world"}]
     )
-    print(message.content)
+    print("AINTROPIC RESPONSE: \n", message.content[0].text)
+    print("---------------------------------------")
+    return(message.content[0].text)
 
-generate_code()
+
+def extract_code (response_text):
+    #this will extract code from response
+    import re #assuming code will be enclosed in tripple backticks
+
+     # Try ```python ... ```
+    pattern = r"```python\n(.*?)\n```"
+    code = re.search(pattern, response_text, re.DOTALL)
+    if code:
+        print('EXTRACTED CODE.....')
+        print("PATTERN1: ",code.group(1).strip())
+        return code.group(1).strip()
+
+    pattern = r"```\n(.*?)\n```"
+    code = re.search(pattern, response_text, re.DOTALL)
+    if code:
+        print('EXTRACTED CODE.....')
+        print("PATTERN2: ",code.group(1).strip())
+        return code.group(1).strip()
+    
+
+    return(response_text.strip())
+
+response = generate_code()
+
+extract_code(response)
+
